a388cf534a6f8f956a8557931b510ca34d145046
Leon Machens <leon.machens@googlemail.com>
2019-11-01 15:22:34 +0000

    add lol events (#5)

--- src/pages/Desktop.js
+++ src/pages/Desktop.js
@@ -3,14 +3,19 @@ import GlobalStyle from "../components/GlobalStyle";
 import Header from "../components/Header";
 import { toggleOverlayWindow } from "../api/windows";
 import { registerHotKey } from "../api/settings";
+import { addLolEventListener } from "../api/games";
 
 export default function Desktop() {
+  const [gold, setGold] = React.useState(0);
+
   React.useEffect(() => {
     console.log("register");
     registerHotKey(() => {
       console.log("toggle");
       toggleOverlayWindow();
     });
+
+    addLolEventListener({ setGold });
   }, []);
 
   return (
@@ -18,6 +23,7 @@ export default function Desktop() {
       <GlobalStyle />
       <Header />
       <h1>Overwolf</h1>
+      <p>{gold}</p>
     </>
   );
 }
