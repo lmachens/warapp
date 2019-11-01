b39fb8e08cec7442bb0b97f306e4b6d034340ffc
lmachens <leon.machens@googlemail.com>
2019-11-01 15:22:04 +0000

    add lol events

--- src/pages/Overlay.js
+++ src/pages/Overlay.js
@@ -1,13 +1,21 @@
 import React from "react";
 import GlobalStyle from "../components/GlobalStyle";
 import Header from "../components/Header";
+import { addLolEventListener } from "../api/games";
 
 export default function Overlay() {
+  const [gold, setGold] = React.useState(0);
+
+  React.useEffect(() => {
+    addLolEventListener({ setGold });
+  }, []);
+
   return (
     <>
       <GlobalStyle />
       <Header />
       <h1>Overlay!!</h1>
+      <p>{gold}</p>
     </>
   );
 }
