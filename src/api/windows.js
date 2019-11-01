6fe89a8c26cae3b10f70f8dcb8eb5ba9b8838645
lmachens <leon.machens@googlemail.com>
2019-11-01 14:51:29 +0000

    add hotkey

--- src/api/windows.js
+++ src/api/windows.js
@@ -1,7 +1,37 @@
+const desktopWindowName = "desktop";
+const overlayWindowName = "overlay";
+
 export function closeWindow() {
-  overwolf.windows.close("desktop");
+  overwolf.windows.close(desktopWindowName);
 }
 
 export function dragMoveWindow() {
-  overwolf.windows.dragMove("desktop");
+  overwolf.windows.dragMove(desktopWindowName);
+}
+
+export function toggleOverlayWindow() {
+  overwolf.windows.getWindowState(overlayWindowName, state => {
+    if (state.status === "success") {
+      const windowState = state.window_state_ex;
+      if (windowState === "minimized" || windowState === "closed") {
+        openOverlayWindow();
+      } else if (windowState === "normal" || windowState === "maximized") {
+        closeOverlayWindow();
+      }
+    }
+  });
+}
+
+export function openOverlayWindow() {
+  overwolf.windows.obtainDeclaredWindow(overlayWindowName, result => {
+    const windowId = result.window.id;
+    overwolf.windows.restore(windowId, result => {
+      if (result.status !== "success") {
+      }
+    });
+  });
+}
+
+export function closeOverlayWindow() {
+  overwolf.windows.minimize(overlayWindowName, console.log);
 }
--- src/pages/Desktop.js
+++ src/pages/Desktop.js
@@ -1,8 +1,18 @@
 import React from "react";
 import GlobalStyle from "../components/GlobalStyle";
 import Header from "../components/Header";
+import { toggleOverlayWindow } from "../api/windows";
+import { registerHotKey } from "../api/settings";
 
 export default function Desktop() {
+  React.useEffect(() => {
+    console.log("register");
+    registerHotKey(() => {
+      console.log("toggle");
+      toggleOverlayWindow();
+    });
+  }, []);
+
   return (
     <>
       <GlobalStyle />
