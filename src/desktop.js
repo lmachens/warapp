1db2987952b08ecee0fd940b38484288a507e59e
lmachens <leon.machens@googlemail.com>
2019-11-01 11:37:02 +0000

    add react

---
+++ src/desktop.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Welcome from './pages/Welcome';
+
+const app = document.getElementById('app');
+
+ReactDom.render(<Welcome />, app);
---
+++ src/pages/Welcome.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Welcome() {
+    return <h1 style={{ color: "red" }}>Overwolf</h1>;
+}
